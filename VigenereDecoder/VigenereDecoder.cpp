2f816d7bb78a0283da942792658627b64889fec3
unknown <Moah@Victorique.(none)>
2014-07-29 21:39:38 +0200

    Slight clean up to make the code slightly less bad: - added some comments - renamed some variables - removed "26" as a magic number

--- VigenereDecoder/VigenereDecoder.cpp
+++ VigenereDecoder/VigenereDecoder.cpp
@@ -5,45 +5,60 @@
 
 using namespace std;
 
+const int ALPHABET_SIZE = 26;
+
 char letterToNum(const char letter) {
-    return (letter - 'a') % 26 + 1;
+    // converts a letter to its numeric value in the alphabet
+    return (letter - 'a') % ALPHABET_SIZE + 1;          // + 1 to stay consistent with codes where A == 1 is important
+}
+
+char numToLetter(const char num) {
+    // converts number value into the corresponding letter in the alphabet
+    return ((num - 1 + ALPHABET_SIZE) % ALPHABET_SIZE) + 'a';
 }
 
 char difLetterNums(const char lhs, const char rhs) {
+    // substracts a numeric value representing a letter from another
     char returnValue = lhs - rhs;
     while (returnValue < 0) {
-        returnValue += 26;
+        returnValue += ALPHABET_SIZE;
     }
     return returnValue;
 }
 
-char numToLetter(const char num) {
-    return num - 1 + 'a';
-}
-
-string simpleKeyDecode(const string message, const string decoder) {
+string simpleKeyDecode(const string message, const string key) {
+    // decodes a crypted message by "substracting" the key from the coded message
+    // if the coded message is longer than the key, the function cycles back at the start of the key
+    // WARNING: the function doesn't handle non-lowercase letters in the key
     auto currentCode = message.begin();
-    auto currentKey = decoder.begin();
+    auto currentKey = key.begin();
 
     string decoded = "";
 
     while (currentCode != message.end()) {
-        char letter = tolower(*currentCode);
+        char letter = tolower(*currentCode); // only handling lowercase letter
+
+        // skips character that are not letters
         if (letter >= 'a' && letter <= 'z') {
             char codeLetter = letterToNum(letter);
             char keyLetter = letterToNum(*currentKey);
             char decodedLetter = numToLetter(difLetterNums(codeLetter, keyLetter));
+
+            // check if we need to restore the original case of the letter
             if (letter == *currentCode) {
                 decoded.push_back(decodedLetter);
             }
             else {
                 decoded.push_back(toupper(decodedLetter));
             }
-            if (++currentKey == decoder.end()) {
-                currentKey = decoder.begin();
+
+            // check if we reached the end of the key, cycles back to the start if needed
+            if (++currentKey == key.end()) {
+                currentKey = key.begin();
             }
         }
         else {
+            // in case of any other character, preserve it as is
             decoded.push_back(*currentCode);
         }
         ++currentCode;
